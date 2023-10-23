36e60a474842adf80f13f3c0c7013776954bb7bd
hansun-hub <hansun33@naver.com>
2023-10-23 22:03:41 +0900

    2023-10-23 SWEA 문제 풀이 D2-1959

---
+++ SWEA/D2/1959.py
@@ -0,0 +1,19 @@
+t=int(input())
+for tc in range(1,t):
+    n,m=map(int,input().split())
+    A = list(map(int,input().split()))
+    B = list(map(int,input().split()))
+
+    if n>m:
+        n,m = m,n
+        A,B = B,A
+
+    maxSum=0
+    for i in range(m-n+1):
+        temp=0
+        for j in range(n):
+            temp+=A[j]*B[j+i]
+        if temp>maxSum:
+            maxSum=temp
+
+    print(f'#{tc} {maxSum}')
