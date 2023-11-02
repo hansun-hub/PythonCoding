09d145f9059d18532a549f97ed7957fa41f72205
hansun-hub <hansun33@naver.com>
2023-11-02 16:57:57 +0900

    2023-11-2 swea 문제 풀이 D3-1244

--- 연습.py
+++ 연습.py
@@ -1,20 +1,7 @@
-def lastday(month):
-    days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
-    return days[month]
+visited = {}
+visited[('hansun',1)]=0
+visited[('dumi',2)]=0
+visited[('gun',3)]=0
 
-
-def daysreturn(m1,d1,m2,d2):
-    days=0
-    if m1 == m2:
-        days=d2-d1+1
-    else:
-        days += (lastday(m1) - d1) + 1
-        for i in range(m1+1,m2):
-            days+=lastday(i)
-        days += d2
-    return days
-
-t=int(input())
-for tc in range(1,t+1):
-    m1,d1,m2,d2 = map(int,input().split())
-    print(f'#{tc} {daysreturn(m1,d1,m2,d2)}')
+if visited.get(('hansun',1),1):
+    print("마 뭔소리고")
