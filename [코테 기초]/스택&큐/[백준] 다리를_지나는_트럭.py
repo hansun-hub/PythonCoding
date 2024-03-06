0a07c751eb0f23f5006dbf9edd86cfd15659d66b
hansun-hub <hansun33@naver.com>
2024-03-06 20:13:16 +0900

    2024-03-06 - 코테 기초 스택&큐

---
+++ [코테 기초]/스택&큐/[백준] 다리를_지나는_트럭.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+
+def solution(bridge_length, weight, truck_weights):
+    bridge = deque([0] * bridge_length)
+    truck_weights = deque(truck_weights)
+    time = 0
+
+    currentWeight = 0
+    while len(truck_weights) != 0:
+        time += 1
+        currentWeight -= bridge.popleft()
+
+        if currentWeight + truck_weights[0] <= weight:
+            currentWeight += truck_weights[0]
+            bridge.append(truck_weights.popleft())
+        else:
+            bridge.append(0)
+    time += bridge_length
+    return time
